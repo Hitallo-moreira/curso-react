f6787f07fa810803f6d78b21c333717769ccd4be
HitalloFeitosa <hitallo.feitosa@oficialfarma.com.br>
2024-02-13 17:14:57 -0300

    feat: add favorites context

--- react/src/routes.jsx
+++ react/src/routes.jsx
@@ -5,16 +5,19 @@ import Footer from "./components/Footer/index";
 import Home from "./pages/Home";
 import Favorites from "./pages/Favorites";
 import Container from "./components/Container/index";
+import FavoritesProvider, { FavoritesContext } from "context/Favorites";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Header />
       <Container>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/favoritos" element={<Favorites />} />
-        </Routes>
+        <FavoritesProvider>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/favoritos" element={<Favorites />} />
+          </Routes>
+        </FavoritesProvider>
       </Container>
       <Footer />
     </BrowserRouter>
